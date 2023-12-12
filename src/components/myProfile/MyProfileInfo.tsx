b529fd766bafb9ad27dae5914aa1e153710e4d69
이규정 <115642699+LKJ970524@users.noreply.github.com>
2023-12-12 15:50:51 +0900

    🎨 style: Profile 헤더 스타일링 (#25)
    Profile 헤더 스타일링 완료하였습니다.

--- src/components/myProfile/MyProfileInfo.tsx
+++ src/components/myProfile/MyProfileInfo.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 export const MyProfileInfo = () => {
   return (
     <>
-      <div className='bg-mainGreen h-174 w-full'>
-
+      <div className='bg-mainGreen h-174 w-full flex'>
+        <img className='w-90 h-90 mt-60 ml-42 bg-mainGray' src="" alt="" />
+        <div className='mt-72 ml-16 text-mainBlack'>
+          <div className='text-20'>닉네임</div> {/* :id  닉네임 들어가면 됩니다. */}
+          <div className='text-16'>이메일</div> {/* :id  Email들어가면 됩니다. */}
+          <div className='text-16'>자기소개</div> {/* :id  자기소개 들어가면 됩니다. */}
+        </div>
       </div>
     </>
   )
