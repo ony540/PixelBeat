c76f4f4cff2fb1ba94108387bad1c075fa74f177
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-12-13 17:27:26 +0900

    ✨ feat: home에서 미리듣기 재생 구현 (#31)

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -10,7 +10,7 @@ export const NavBar = () => {
   }
 
   return (
-    <div className="fixed bottom-0 mobile:w-[390px] pt-19 py-23 desktop:w-[720px] bg-mainBlack">
+    <div className="fixed bottom-0 mobile:w-[390px] pt-19 py-23 desktop:w-[720px] bg-mainBlack h-66">
       <nav className="flex justify-around items-center desktop:px-40">
         {NAV_BAR_TEXT.map(({ icon: IconComponent, path }, idx) => (
           <div
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { Header, NavBar, PlayBar } from '@/components'
 import { Banner, SimilarUserList, TopTrackList } from '@/components/home'
+import Portal from '@/utils/portal'
+import { useNowPlayStore } from '@/zustand'
 
 export const Home = () => {
+  const currentTrack = useNowPlayStore(state => state.currentTrack)
   return (
     <div className="relative h-screen overflow-y-auto">
       <Header />
@@ -9,6 +12,11 @@ export const Home = () => {
       <SimilarUserList />
       <TopTrackList />
       <NavBar />
+      <Portal>
+        {currentTrack && (
+          <PlayBar propsClass="bottom-66 border-b border-mainGray" />
+        )}
+      </Portal>
     </div>
   )
 }
