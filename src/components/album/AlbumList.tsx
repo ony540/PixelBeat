bf026aa9bfa7ad9c8b3bede867b41b9e05333cea
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-12-20 10:21:18 +0900

    ✨ feat: 가수, 앨범 페이지 내 현재 재생목록 추가 기능 구현 (#54)

--- src/components/album/AlbumList.tsx
+++ src/components/album/AlbumList.tsx
@@ -1,7 +1,46 @@
-import { TrackItem } from '..'
+import Portal from '@/utils/portal'
+import { BottomSheet, TrackItem } from '..'
+import { useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addNowPlayTracklistTable } from '@/api'
+import { useModal } from '@/hooks'
+import { useNowPlayStore, useUserStore } from '@/zustand'
+import { Track } from '@/types'
 
 export const AlbumList = ({ album_list }) => {
-  if (!album_list) return null
+  const addTrackToNowPlay = useNowPlayStore(state => state.addTrackToNowPlay)
+  const { modalType, closeModal } = useModal()
+  const queryClient = useQueryClient()
+  const userInfo = useUserStore(state => state.userInfo)
+  const [selectedTrack, setSelectedTrack] = useState<Track>()
+
+  const addNowPlayTracklistTableMutation = useMutation({
+    mutationFn: addNowPlayTracklistTable,
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ['profiles from supabase', userInfo.id]
+      })
+    },
+    onError(error) {
+      console.log(error)
+    }
+  })
+
+  //재생목록에 추가하기
+  const handleClickLModalItem = track => {
+    addTrackToNowPlay(track)
+
+    //로그인 사용자일 경우 db에 useMutation 해야함
+    if (userInfo.id) {
+      addNowPlayTracklistTableMutation.mutateAsync({
+        prevNowPlayTracklist: userInfo.nowplay_tracklist,
+        track,
+        userId: userInfo.id
+      })
+    }
+
+    closeModal()
+  }
 
   const albumData = {
     id: album_list.id,
@@ -17,9 +56,15 @@ export const AlbumList = ({ album_list }) => {
             <TrackItem
               key={item.id}
               data={{ ...item, album: albumData }}
+              setSelectedTrack={setSelectedTrack}
             />
           ))}
       </ul>
+      <Portal>
+        {modalType === 'trackMore' && (
+          <BottomSheet onClick={() => handleClickLModalItem(selectedTrack)} />
+        )}
+      </Portal>
     </div>
   )
 }
--- src/components/common/item/TrackItem.tsx
+++ src/components/common/item/TrackItem.tsx
@@ -1,18 +1,44 @@
 import { CirclePlaySmall, MoreIcon } from '@/assets'
 import { useModal } from '@/hooks'
 import { Track } from '@/types'
-import { useNowPlayStore } from '@/zustand'
+import { useNowPlayStore, useUserStore } from '@/zustand'
 import { useNavigate } from 'react-router-dom'
 import defaultAlbumImg from '@/assets/imgs/default_album_artist.png'
-import Portal from '@/utils/portal'
-import { BottomSheet } from '..'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addCurrentTrackTable } from '@/api'
 
-export const TrackItem = ({ data }: any) => {
+export const TrackItem = ({
+  data,
+  setSelectedTrack
+}: {
+  data: Track
+  setSelectedTrack: (track: Track) => void
+}) => {
   const navigate = useNavigate()
 
-  const { openModal, closeModal, modalType, setModalType } = useModal()
+  const { openModal } = useModal()
   const setCurrentTrack = useNowPlayStore(state => state.setCurrentTrack)
   const addTrackToNowPlay = useNowPlayStore(state => state.addTrackToNowPlay)
+  const setIsPlaying = useNowPlayStore(state => state.setIsPlaying)
+  const setNowPlayStore = useNowPlayStore(state => state.setNowPlayStore)
+  const userInfo = useUserStore(state => state.userInfo)
+  const setUserInfo = useUserStore(state => state.setUserInfo)
+  const queryClient = useQueryClient()
+
+  //현재재생목록에 추가 및 지금 재생
+  const addCurrentTrackTableMutation = useMutation({
+    mutationFn: addCurrentTrackTable,
+    onSuccess(data) {
+      queryClient.invalidateQueries({
+        queryKey: ['profiles from supabase', userInfo.id]
+      })
+      setUserInfo(data)
+      setNowPlayStore(data.nowplay_tracklist)
+    },
+    onError(error) {
+      console.log(error)
+    }
+  })
 
   const handleClickAlbum = (id: string) => {
     navigate(`/album/${id}`)
@@ -25,73 +51,62 @@ export const TrackItem = ({ data }: any) => {
   const handleClickPlayButton = (track: Track) => {
     addTrackToNowPlay(track)
     setCurrentTrack(track)
+    setIsPlaying(true)
+    //로그인 유저 db update
+    if (userInfo.id) {
+      addCurrentTrackTableMutation.mutateAsync({
+        prevNowPlayTracklist: userInfo.nowplay_tracklist,
+        track,
+        userId: userInfo.id
+      })
+    }
   }
 
   const handleClickMoreButton = () => {
     openModal('trackMore')
-  }
-
-  const handleClickLModalItem = (e, track) => {
-    //로그인 사용자일 경우 db에 useMutation 해야함
-    if (e.target.innerText === '재생목록에 추가하기') {
-      addTrackToNowPlay(track)
-      closeModal()
-    } else {
-      //내 음악영수증 리스트 보여주기 (bottomSheet 내에서 구현)
-      setModalType('myBillList')
-    }
+    setSelectedTrack(data)
   }
 
   return (
-    <>
-      <li className="border-1 border-b-0 flex datas-center gap-10 hover:bg-mainGray300 relative group">
-        <img
-          onClick={() => handleClickAlbum(data.album.id)}
-          className="mobile:w-50 mobile:h-51 mr-4
+    <li className="border-1 border-b-0 flex datas-center gap-10 hover:bg-mainGray300 relative group">
+      <img
+        onClick={() => handleClickAlbum(data.album.id)}
+        className="mobile:w-50 mobile:h-51 mr-4
                  desktop:w-65 desktop:h-66 cursor-pointer"
-          src={
-            data.album.images[1] ? data.album.images[1].url : defaultAlbumImg
-          }
-          alt={`${data.name}.img`}
-        />
-        <div className="flex flex-col overflow-hidden justify-center w-214 desktop:w-450 leading-[1.2] desktop:text-20 truncate">
-          <p className={data.name.length > 20 ? 'text-flow-on-hover' : ''}>
-            {data.name}
-          </p>
-          <p className={data.artists.length > 2 ? 'text-flow-on-hover' : ''}>
-            {data.artists.map((artist, idx) => (
-              <span
-                key={idx}
-                className="cursor-pointer hover:underline"
-                onClick={() => handleClickAritst(artist.id)}>
-                {artist.name}
-                {idx < data.artists.length - 1 && ', '}
-              </span>
-            ))}
-          </p>
-        </div>
+        src={data.album.images[1] ? data.album.images[1].url : defaultAlbumImg}
+        alt={`${data.name}.img`}
+      />
+      <div className="flex flex-col overflow-hidden justify-center w-214 desktop:w-450 leading-[1.2] desktop:text-20 truncate">
+        <p className={data.name.length > 20 ? 'text-flow-on-hover' : ''}>
+          {data.name}
+        </p>
+        <p className={data.artists.length > 2 ? 'text-flow-on-hover' : ''}>
+          {data.artists.map((artist, idx) => (
+            <span
+              key={idx}
+              className="cursor-pointer hover:underline"
+              onClick={() => handleClickAritst(artist.id)}>
+              {artist.name}
+              {idx < data.artists.length - 1 && ', '}
+            </span>
+          ))}
+        </p>
+      </div>
 
-        {data.preview_url && (
-          <button
-            type="button"
-            onClick={() => handleClickPlayButton(data)}
-            className="absolute top-[50%] translate-y-[-50%] right-42">
-            <CirclePlaySmall isWhite />
-          </button>
-        )}
+      {data.preview_url && (
         <button
           type="button"
-          onClick={handleClickMoreButton}
-          className="absolute top-[50%] translate-y-[-50%] right-10">
-          <MoreIcon />
+          onClick={() => handleClickPlayButton(data)}
+          className="absolute top-[50%] translate-y-[-50%] right-42">
+          <CirclePlaySmall isWhite />
         </button>
-      </li>
-      <Portal>
-        {modalType === 'trackMore' ||
-          (modalType === 'myBillList' && (
-            <BottomSheet onClick={e => handleClickLModalItem(e, data)} />
-          ))}
-      </Portal>
-    </>
+      )}
+      <button
+        type="button"
+        onClick={handleClickMoreButton}
+        className="absolute top-[50%] translate-y-[-50%] right-10">
+        <MoreIcon />
+      </button>
+    </li>
   )
 }
