d46a9f65bf69764a9ca862e96bb4a8a96a5c8575
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-12-20 10:09:54 +0900

    ✨ feat: useConfirm 구현 (#54)

---
+++ src/constants/confirmText.ts
@@ -0,0 +1,20 @@
+export const CONFIRM_TYPE = Object.freeze({
+  delete: ['삭제하시겠습니까?', '삭제'],
+  edit: ['수정하시겠습니까?', '수정'],
+  addOwnPlaylist: [
+    `음악서랍에
+  저장하시겠습니까?`,
+    '저장'
+  ],
+  logout: ['로그아웃하시겠습니까?', '확인'],
+  loginInduce: [
+    `로그인이 필요한 서비스입니다.
+  로그인 하시겠습니까?`,
+    '확인'
+  ],
+  alreadyOwnPlaylist: ['이미 저장된 영수증입니다'],
+  loginFail: [
+    `로그인에 실패했습니다.
+  다시 한번 확인해주세요.`
+  ]
+})
