d550de200cceac24384f074671611c7f44455543
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-12-21 11:19:56 +0900

    🎨 style: 에러컴포넌트 수정 (#62)

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,12 +1,19 @@
 import { ERROR_TEXTS } from '@/constants'
+import { useNavigate } from 'react-router-dom'
+import { SmallButton } from '.'
 
 export const ErrorComponent = () => {
+  const navigate = useNavigate()
   return (
-    <div className="layout-screen-width outline outline-mainGreen">
+    <div className="layout-screen-width outline text-center pt-60 text-16 leading-[1.5]">
       <header>{ERROR_TEXTS.headerText}</header>
       <div>{ERROR_TEXTS.apologyText}</div>
       <div>{ERROR_TEXTS.errorText}</div>
-      <footer>{ERROR_TEXTS.returnText}</footer>
+      <SmallButton
+        onClick={() => navigate('/home')}
+        text={ERROR_TEXTS.returnText}
+        propsClass="w-150 mt-20 relative"
+      />
     </div>
   )
 }
